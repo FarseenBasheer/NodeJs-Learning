a4506a37fa209191af6ec9cad1c7b073cad6166f
Farseen <farseen.basheer@terrificminds.com>
2024-07-02 22:18:17 +0530

    Route setting

--- ShoppingCart/README.md
+++ ShoppingCart/README.md
@@ -9,4 +9,8 @@ https://www.w3schools.com/mongodb/index.php
 hns defined in app.js
 npm install express-handlebars
 
+For image file upload,
+add enctype="multipart/form-data" in form
+
+npm i express-fileupload
 
---
+++ ShoppingCart/routes/admin.js
@@ -0,0 +1,45 @@
+var express = require('express');
+const { route } = require('./admin');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  let products=[
+    {
+      name: "IPHONE 11",
+      category: "mobile",
+      description: "This is a good phone",
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTtAYqQmgIlElczj3o8gOy27sWgC_-CkbHqmA&s"
+    },
+    {
+      name: "One Plus 7T",
+      category: "mobile",
+      description: "This is a good phone",
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR4yXNjbXvoGoyxyfHfnWlDnhCZ5unS2ewz2A&s"
+    },
+    {
+      name: "OOPO 10X",
+      category: "mobile",
+      description: "This is a good phone",
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUJjHeX8s-9YC5RZQsCsgQ6pXNGv0huD42Xw&s"
+    },
+    {
+      name: "MI Note 9 Pro",
+      category: "mobile",
+      description: "This is a good phone",
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRxhgrw1vn4uQtGdMUNjiIBazcnkVUN1emHdg&s"
+    },
+  ];
+  res.render('admin/view-products', {admin:true, products});
+});
+
+router.get('/add-product', function(req, res){
+  res.render('admin/add-product', {admin: true})
+})
+
+router.post('/add-product',(req,res)=>{
+  console.log(req.body);
+  console.log(req.files.Image);
+})
+
+module.exports = router;
